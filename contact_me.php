bda00e82d957cfeaac51902a15786a658f697650
GehadAdell <email>
2022-11-01 15:06:35 +0200

    first commit

---
+++ contact_me.php
@@ -0,0 +1,56 @@
+<?php
+include './shared/header.php';
+include './shared/navbar.php';
+include './shared/aside.php';
+?>
+
+<div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid px-4">
+
+            <div class="pagetitle">
+                <h1 class="">Contact me</h1>
+                <nav>
+                    <ol class="breadcrumb" style="height: 40px; align-items:center;">
+                        <li class="breadcrumb-item"><a href="/ODC8/dashboard.php">Dashboard</a></li>
+                        <li class="breadcrumb-item active">Contact me</li>
+                    </ol>
+                </nav>
+            </div>
+
+            <div class=" align-items-center align-self-center" >
+                <div class="card-body profile-card pt-4 d-flex flex-column align-items-center" style="width: 600px;">
+
+                    <img src="./uploads/1667306649WhatsApp Image 2022-11-01 at 2.29.06 PM.jpeg" alt="Profile" class="rounded-circle" style="height: 250px; width: 250px;">
+                    <h2>Gehad Adel</h2>
+                    <p> Back-End Developer
+                    </p>
+                    <p> email: gehad0@gmail.com
+                    </p>
+
+                    <div class="social-links mt-2">
+
+                        <a href="https://www.facebook.com/ana.b.rawsh/" class="facebook" >
+                            <i class="fa-brands fa-facebook" style="height: 35px; "></i>
+                        </a>
+                        <a href="https://instagram.com/gehad_adell?igshid=YmMyMTA2M2Y=" class="instagram">
+                            <i class="fa-brands fa-instagram" style="height: 35px; color:deeppink;"></i>
+                        </a>
+                        <a href="https://github.com/GehadAdell" class="github">
+                        <i class="fa-brands fa-github" style="height: 35px; color:darkslategrey;"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/gehad-adel-772b22202" class="linkedin">
+                            <i class="fa-brands fa-linkedin" style="height: 35px; "></i>
+                        </a>
+                
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </main>
+</div>
+<?php
+include './shared/footer.php';
+?>
